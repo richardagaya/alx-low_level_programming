ce06eb56fce15a7954a94f6461d5f5f0e7d7d849
richardagaya <richardagaya278@gmail.com>
2023-03-21 13:56:21 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,20 @@
-#include <stdio.h>
+#include "main.h"
 
-/** main - Entry point
+/** main - Entry point of my code.
  *
- * Return: Always 0 (success)
+ * Return: Always 0
  */
+
 int main(void)
 {
-	putchar("hello world/n");
-	return(0);
+_putchar("_");
+_putchar("p");
+_putchar("u");
+_putchar("t");
+_putchar("c");
+_putchar("h");
+_putchar("a");
+_putchar("r");
+_putchar("\n");
+return (0);
 }
