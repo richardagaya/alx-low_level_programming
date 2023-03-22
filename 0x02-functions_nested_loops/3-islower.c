51f88710ac80fd596cc52ecb329e61a339aef076
richardagaya <richardagaya278@gmail.com>
2023-03-22 09:47:48 -0700

    is lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 
 /**
  * _islower - Checks for lowercase character
- * @c: The character to be checked 
+ * @c:The character to be checked 
  * Return: 1 for lowercase character or 0 for anything else
  */
 int _islower(int c)
@@ -10,7 +10,7 @@ int _islower(int c)
 
 	if (c >= 97 && c <= 122)
 	{
-	return(1);
+	return (1);
 	}
 	return (0);
 }
