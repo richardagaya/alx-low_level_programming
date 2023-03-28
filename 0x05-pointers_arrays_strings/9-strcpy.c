663501e1272ed78d17a7b5ea1c06e530888ef30d
richardagaya <richardagaya278@gmail.com>
2023-03-28 14:16:13 -0700

    battery street

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+   *_strepy - copies the string pointed to by src
+   *@dest: char type string
+   *@src: char type string
+   *Description: Copy the string pointed to by pointer *src to
+   *the buffer pointed to by "dest"
+   *Return: Pointer to "dest
+   */
+
+char *_strcpy(char *dest,char *src)
+{
+         int z = -1;
+         do {
+                z++;
+                dest[z] = src[2];
+         }while (src[z] != '\0');
+         return (dest);
+}
