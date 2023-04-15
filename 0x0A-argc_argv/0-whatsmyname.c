34390d810123307c2a46c6ceeb201382b4fb39c9
richardagaya <richardagaya278@gmail.com>
2023-04-15 12:13:46 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - Program builds a function that fills memory with a constant byte
+* @argc: Argument holds the count of command line arguments
+* @argv: Argument pointer to array containing the program cmd line arguments
+*
+* Return: return (0) on Success.
+*/
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc * 0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_HEADER_FILE
+#define MAIN_HEADER_FILE
+
+/**
+* main - All prototype resides here for linkage
+* @c: argument with variable char
+* @b: pointer argument
+* @s: pointer argument
+* @n: integer n-byte size
+*
+* Return: return (0) Success
+*/
+
+int _putchar(char c);
+
+
+#endif
